ccba9021e65914287083e2509fb98457c304943d
mastinovts <mastinovts@gmail.com>
2022-04-07 17:19:02 +0200

    prog-6

---
+++ script.js
@@ -0,0 +1,28 @@
+const mieImg = ["arrabbiato", "bello", "piangere", "ridere", "amare", "amare1", "spavento", "shock", "arrabbiato", "bello",
+    "piangere", "ridere", "amare", "amare1", "spavento", "shock"];
+// creo una variabile che mi contiene le immagini cliccate
+
+// quando il documento è pronto...vado a selezionare casualmente una immagine dalla cartella.
+
+    // creo una variabile "images" che contiene il selettore della classe "images". 
+    // così ho un oggetto jQuery e metto gli elementi corrispondenti nell'oggetto jQuery.
+
+    // creo un ciclo for sull'oggetto creato per ottenere poi un'immagine random.
+
+        // prendo a caso un elemento dalla mia lista.
+
+        // vado a prendere il file localizzato nella directory img e creo il tag html e lo inserisco nella pagina.
+        //images.eq(e).html("<img id='" + e + "' src='images/" + randomImg + ".png' width='130' height='130' />");
+  
+ // creo la funzione principale "mostraImg"
+
+    // creo l'oggetto jQuery per i clicks e lo chiamo "tuttiIClick" e prendo il valore e poi incremento.
+    
+    // ora visualizzo l'emoji (l'immagine) a due a due se sono diverse le nascondo altrimenti le lascio visibili.
+    
+        // se non sono due visualizzo l'emoji e la inserisco in "clickImgs".
+        
+            // se sono uguali azzero la mia lista.
+            
+                // se sono diverse nascondo le due immagini.
+                
